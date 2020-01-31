e0eca55af619e3038c0dc465c26f33fe1139d119
Kirsten Querna <kirsten@querna.org>
2020-01-31 14:22:32 -0800

    reduce getIssueEpics

--- src/sprint.actions.js
+++ src/sprint.actions.js
@@ -46,38 +46,27 @@ async function status({ team, id }) {
 
 async function getIssueEpics(epicIssues) {
   const points = getCurrentContext().points;
-  // map epic issues - distinct epics to total epic issue points in sprint
-  let mappedPoints = epicIssues.issues.reduce(function(map, issue) {    
+  // Summarize epic issues - distinct epics to total epic issue points in sprint & summary
+  let epicSummary = epicIssues.issues.reduce(function(map, issue) {    
     // TO-DO: handle error if issue does not have an epic && convert to single reduce function
-    let key = issue.fields.epic.key
-    // let name = issue.fields.epic.name || issue.fields.epic.fields.name
-    let epicPoints = +issue.fields[points]
-    map[key] = (map[key] || 0) + epicPoints
-    return map
-  }, {})
+    const key = issue.fields.epic.key;
+    const sprintPoints = +issue.fields[points];
 
-  // group by epic key to get epic name 
-  const groupBy = (array, key) => {
-    return array.reduce((result, currentValue) => {
-      (result[currentValue.fields.epic.key] = result[currentValue.fields.epic.key] || [] ).push(
-        {
-          summary: currentValue.fields.epic.name || currentValue.fields.epic.fields.id
-        }
-      );
-      return result;
-    }, {});
-  };
-  // get list of epic names for issues 
-  const epicGroupedByKey = groupBy(epicIssues.issues, "fields.epic.key");
+    map[key] = map[key] || {};
+    map[key].sprintPoints = (map[key].sprintPoints + sprintPoints) || sprintPoints;
+    map[key].summary = issue.fields.epic.name || issue.fields.epic.fields.name;
+
+    return map;
+  }, {});
 
   // final output object w/ all needed fields
   const array = await Promise.all(
-    Object.keys(mappedPoints).map(async function(name) {
+    Object.keys(epicSummary).map(async function(name) {
       let epicData = await statusEpic({ id: name });
       return {
         key: name,
-        displayName: epicGroupedByKey[name][0].summary,
-        points: mappedPoints[name],
+        displayName: epicSummary[name].summary,
+        points: epicSummary[name].sprintPoints,
         total: epicData.epics[0].totalPoints,
         completed: epicData.epics[0].completedPoints,
       }
